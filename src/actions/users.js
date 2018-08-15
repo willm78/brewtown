810c923e5d3e67a061e011ea23172c9463d4d6d2
Will <will.mcdaniell@tamu.edu>
2018-08-14 20:11:51 -0500

    added normalizr

--- src/actions/users.js
+++ src/actions/users.js
@@ -24,3 +24,29 @@ export const loadUser = (userName, requiredFields = []) => (
 
   return dispatch(fetchUser(login));
 };
+
+export const USER_REQUEST = "USER_REQUEST";
+export const USER_SUCCESS = "USER_SUCCESS";
+export const USER_FAILURE = "USER_FAILURE";
+
+const fetchUser = userName => ({
+  [CALL_API]: {
+    types: [USER_REQUEST, USER_SUCCESS, USER_FAILURE],
+    schema: Schemas.USER
+  }
+});
+
+export const loadUser = (userName, requiredFields = []) => (
+  dispatch,
+  getState
+) => {
+  const user = getState().entities.users.byId[userName];
+
+  // TODO: This never reloads if fields are already present in the state
+  if (user && requiredFields.every(key => user.hasOwnProperty(key))) {
+    return null;
+  }
+
+  return dispatch(fetchUser(login));
+};
+
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,7 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { AsyncStorage } from "react-native";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
